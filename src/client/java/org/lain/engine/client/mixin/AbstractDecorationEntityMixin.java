6999e8f44190c43a4e8844a605a2e95b8d2eaa19
Трибунал <48561105+lain1wakura@users.noreply.github.com>
2025-12-31 05:23:37 +0300

    feat: рамки не уничтожаются

---
+++ src/client/java/org/lain/engine/client/mixin/AbstractDecorationEntityMixin.java
@@ -0,0 +1,23 @@
+package org.lain.engine.client.mixin;
+
+import net.minecraft.entity.decoration.AbstractDecorationEntity;
+import net.minecraft.entity.decoration.ItemFrameEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AbstractDecorationEntity.class)
+public class AbstractDecorationEntityMixin {
+    @Inject(
+            method = "canStayAttached",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    public void engine$canStayAttached(CallbackInfoReturnable<Boolean> cir) {
+        if (((Object)this) instanceof ItemFrameEntity) {
+            cir.setReturnValue(true);
+            cir.cancel();
+        }
+    }
+}
