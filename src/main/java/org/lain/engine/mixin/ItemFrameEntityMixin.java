93166a0f53fbec8e405d538974243d4630e831ff
Трибунал <48561105+lain1wakura@users.noreply.github.com>
2026-01-13 12:51:30 +0300

    fix: рамки не исчезают в блоках

--- src/main/java/org/lain/engine/mixin/ItemFrameEntityMixin.java
+++ src/main/java/org/lain/engine/mixin/ItemFrameEntityMixin.java
@@ -1,23 +1,20 @@
 package org.lain.engine.mixin;
 
-import net.minecraft.entity.decoration.AbstractDecorationEntity;
 import net.minecraft.entity.decoration.ItemFrameEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(AbstractDecorationEntity.class)
-public class AbstractDecorationEntityMixin {
+@Mixin(ItemFrameEntity.class)
+public class ItemFrameEntityMixin {
     @Inject(
             method = "canStayAttached",
             at = @At("RETURN"),
             cancellable = true
     )
     public void engine$canStayAttached(CallbackInfoReturnable<Boolean> cir) {
-        if (((Object)this) instanceof ItemFrameEntity) {
-            cir.setReturnValue(true);
-            cir.cancel();
-        }
+        cir.setReturnValue(true);
+        cir.cancel();
     }
 }
