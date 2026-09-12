3c7480490946cb7bac5a8826b4054b654ea095af
Трибунал <48561105+lain1wakura@users.noreply.github.com>
2026-09-13 00:17:06 +0300

    build: libs.versions.toml, перераспределение зависимостей

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,56 +1,20 @@
-
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "2.2.21"
-    kotlin("plugin.serialization") version "2.1.0"
-    id("fabric-loom") version "1.16-SNAPSHOT"
-    id("com.gradleup.shadow") version "9.3.0"
+    alias(libs.plugins.loom)
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.shadow)
 }
 
-version = project.property("mod_version") as String
-group = project.property("maven_group") as String
+version = providers.gradleProperty("mod_version").get()
+group = providers.gradleProperty("maven_group").get()
 
 base {
-    archivesName.set(project.property("archives_base_name") as String)
-}
-java {
-    withSourcesJar()
-}
-
-val jdkVersion = 21
-kotlin {
-    jvmToolchain(jdkVersion)
-}
-
-loom {
-    splitEnvironmentSourceSets()
-
-    mods {
-        register("engine") {
-            sourceSet("main")
-            sourceSet("client")
-        }
-    }
-
-    accessWidenerPath = file("src/client/resources/engine.classtweaker")
+    archivesName.set(providers.gradleProperty("archives_base_name").get())
 }
 
 repositories {
-    exclusiveContent {
-        forRepository {
-            maven("https://api.modrinth.com/maven") {
-                name = "Modrinth"
-            }
-        }
-        filter {
-            includeGroup("maven.modrinth")
-        }
-    }
-
-    maven("https://maven.logandark.net")
-
     maven {
         name = "GitHubPackages"
         url = uri("https://maven.pkg.github.com/qbrp/cyberia")
@@ -59,92 +23,64 @@ repositories {
             password = findProperty("gpr.key") as String?
         }
     }
-    maven {
-        name = "Gegy"
-        url = uri("https://maven.gegy.dev")
+    exclusiveContent {
+        forRepository {
+            maven("https://api.modrinth.com/maven")
+        }
+        filter {
+            includeGroup("maven.modrinth")
+        }
     }
-    maven {
-        name = "EngineHub"
-        url = uri("https://maven.enginehub.org/repo/")
-    }
+    maven("https://maven.logandark.net")
+    maven("https://maven.gegy.dev")
+    maven("https://maven.enginehub.org/repo/")
     maven("https://maven.wispforest.io/releases/2412")
     maven("https://jitpack.io")
     maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    mavenCentral()
 }
 
+loom {
+    splitEnvironmentSourceSets()
+    accessWidenerPath = file("src/client/resources/engine.classtweaker")
 
-val transitive by configurations.creating
+    mods {
+        register("engine") {
+            sourceSet("main")
+            sourceSet("client")
+        }
+    }
+}
+
+val shaded by configurations.creating
 
 dependencies {
-    fun shaded(dep: String) {
-        implementation(dep)
-        transitive(dep)
+    fun shaded(dependency: Any) {
+        implementation(dependency)
+        add(shaded.name, dependency)
     }
 
-    val minecraft_version = project.property("minecraft")
-
-    minecraft("com.mojang:minecraft:$minecraft_version")
+    minecraft(libs.minecraft)
     mappings(loom.officialMojangMappings())
-    modImplementation("net.fabricmc:fabric-loader:${project.property("fabric_loader")}")
-    modImplementation("net.fabricmc:fabric-language-kotlin:${project.property("kotlin_loader")}")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:${project.property("fabric_api")}")
+    modImplementation(libs.bundles.fabric)
 
-    // YAML Config
+    modImplementation(libs.graphene)
+    modImplementation(libs.yaml)
+    modImplementation(libs.ui)
+    modImplementation(libs.architectury)
+    modImplementation(libs.adventure)
+    modImplementation(libs.cyberia)
+    modImplementation(libs.cyberia.ecs.api)
+    modImplementation(libs.permissions)
+    modImplementation(libs.female.gender)
+    modLocalRuntime(libs.lambdynlights.runtime)
+    modCompileOnly(libs.bundles.compat)
 
-    modImplementation("maven.modrinth:yaml-config:${project.property("yaml_config")}-fabric")
-    modImplementation("maven.modrinth:ui-lib:${project.property("ui_lib")}-fabric")
-    modApi("maven.modrinth:architectury-api:${project.property("architectury_api")}+fabric")
+    compileOnly(libs.bundles.libraries)
+    testImplementation(libs.fabric.loader.junit)
 
-    // Kyori Adventure
-    val adventurePlatform = project.property("adventure_platform_version")
-    modImplementation(include("net.kyori:adventure-platform-fabric:$adventurePlatform")!!)
-
-    // Kaml
-    include(implementation("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:1.9.0")!!)
-    shaded("com.charleskorn.kaml:kaml:0.104.0")
-    shaded("de.javagl:obj:0.4.0")
-
-    // Secure Storage
-    shaded("com.microsoft:credential-secure-storage:1.0.3")
-
-    // Permission API
-    modImplementation("me.lucko:fabric-permissions-api:${project.property("fabric_permissions_version")}")
-
-    // Game tests
-    testImplementation("net.fabricmc:fabric-loader-junit:${project.property("fabric_loader")}")
-
-    // Тяжелые зависимости
-    val cyberiaDependencyVersion = project.property("cyberia_version")!!
-    modImplementation("org.lain.cyberia:fabric:$cyberiaDependencyVersion")
-    modImplementation("org.lain.cyberia:ecs-api:1.3.4")
-    compileOnly("org.jetbrains.exposed:exposed-core:1.0.0")
-    compileOnly("org.jetbrains.exposed:exposed-jdbc:1.0.0")
-    compileOnly("org.xerial:sqlite-jdbc:3.51.1.0")
-    shaded("org.reflections:reflections:0.10.2")
-
-    // Camera Overhaul
-    val (cameraOverhaulVersion, cameraOverhaulMinecraftVersion) = project.property("camera_overhaul_version")
-        .toString()
-        .split("+")
-        .let { it[0] to it[1] }
-    modCompileOnly("maven.modrinth:cameraoverhaul:${cameraOverhaulVersion}-fabric+mc.$cameraOverhaulMinecraftVersion-plus")
-
-    // Wildfire Gender Mod
-    modImplementation("maven.modrinth:female-gender:5.0.0-Beta.3+1.21.11")
-
-    // API Lamb Dynamic Lights
-    modCompileOnly("dev.lambdaurora.lambdynamiclights:lambdynamiclights-api:${project.property("lambdynamiclights_version")}")
-    modLocalRuntime("dev.lambdaurora.lambdynamiclights:lambdynamiclights-runtime:${project.property("lambdynamiclights_version")}")
-
-    // API WorldEdit
-    modCompileOnly("com.sk89q.worldedit:worldedit-core:${project.property("worldedit_version")}")
-    modCompileOnly("com.sk89q.worldedit:worldedit-fabric-mc$minecraft_version:${project.property("worldedit_version")}")
-
-    // Graphene
-    modImplementation("io.github.trethore:graphene-ui:1.7.2")
-
-    // Lua
-    compileOnly("org.luaj:luaj-jse:3.0.1")
+    implementation(libs.protobuf)
+    include(libs.protobuf)
 }
 
 tasks.test {
@@ -152,19 +88,20 @@ tasks.test {
 }
 
 tasks.processResources {
-    inputs.property("version", project.version)
-    inputs.property("minecraft_version", project.property("minecraft"))
-    inputs.property("loader_version", project.property("fabric_loader"))
-    inputs.property("cyberia_version", project.property("cyberia_version"))
-    filteringCharset = "UTF-8"
+    val version = version
+    inputs.property("version", version)
+    inputs.property("minecraft_version", libs.versions.minecraft.get())
+    inputs.property("loader_version", libs.versions.fabric.loader.get())
+    inputs.property("kotlin_loader_version", libs.versions.fabric.kotlin.get())
+    inputs.property("cyberia_version", libs.versions.cyberia.version.get())
 
     filesMatching("fabric.mod.json") {
         expand(
-            "version" to project.version,
-            "minecraft_version" to project.property("minecraft")!!,
-            "loader_version" to project.property("fabric_loader")!!,
-            "kotlin_loader_version" to project.property("kotlin_loader")!!,
-            "cyberia_version" to project.property("cyberia_version")!!
+            "version" to version,
+            "minecraft_version" to libs.versions.minecraft.get(),
+            "loader_version" to libs.versions.fabric.loader.get(),
+            "kotlin_loader_version" to libs.versions.fabric.kotlin.get(),
+            "cyberia_version" to libs.versions.cyberia.version.get(),
         )
     }
 }
@@ -173,11 +110,9 @@ tasks.shadowJar {
     exclude("kotlin/**")
     exclude("META-INF/kotlin*")
     exclude("*kotlin")
-
     from(sourceSets.main.get().output)
     from(sourceSets["client"].output)
-
-    configurations = listOf(transitive)
+    configurations = listOf(shaded)
 }
 
 tasks.remapJar {
@@ -187,19 +122,28 @@ tasks.remapJar {
 
 tasks.withType<JavaCompile>().configureEach {
     options.encoding = "UTF-8"
-    options.release.set(jdkVersion)
+    options.release = 21
 }
 
-tasks.withType<KotlinCompile> {
+kotlin {
     compilerOptions {
-        jvmTarget.set(JvmTarget.fromTarget(jdkVersion.toString()))
+        jvmTarget = JvmTarget.JVM_21
         freeCompilerArgs.add("-Xcontext-parameters")
     }
 }
 
+java {
+    withSourcesJar()
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
 tasks.jar {
-    from("LICENSE") {
-        rename { "${it}_${project.base.archivesName.get()}" }
+    val projectName = project.name
+    inputs.property("projectName", projectName)
+
+    from("LICENSE.txt") {
+        rename { "${it}_$projectName" }
     }
     duplicatesStrategy = DuplicatesStrategy.WARN
 }
